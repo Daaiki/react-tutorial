d3849c6e0b02bc3f0160bd9e67758dc660d6049e
Daaiki <daaikii@outlook.jp>
2021-05-03 20:42:16 +0900

    add next/head

--- next-ts/src/pages/index.tsx
+++ next-ts/src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const IndexPage = () => {
   return (
     <>
+      <Head>
+        <title>Create React App</title>
+      </Head>
       <h1>
         Read <Link href="/posts/first-post"><a>this page</a></Link> 
       </h1>
