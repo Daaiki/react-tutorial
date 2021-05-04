62a5e5d5888806822393963a749c8c1a12bd258b
Daaiki <daaikii@outlook.jp>
2021-05-04 14:44:29 +0900

    add global css

--- next-ts/src/pages/_app.tsx
+++ next-ts/src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
-import '../styles/global.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Component {...pageProps} />
+      <style global jsx>{`
+        :root {
+          --link-color: #1A0DAB;
+        }
+        a {
+          color: var(--link-color);
+        }
+        a:hover {
+          border-bottom: 0.2rem solid var(--link-color);
+        }
+      `}</style>
     </>
   )
 }
