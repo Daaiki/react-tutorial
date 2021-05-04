ccce24fc4af652979131a6e5799117c15ad8d061
Daaiki <daaikii@outlook.jp>
2021-05-04 21:58:54 +0900

    display posts data

--- next-ts/src/pages/index.tsx
+++ next-ts/src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { getSortedPostsData } from '../lib/posts'
+import { Posts } from '../types/types'
 
-const IndexPage = () => {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+} 
+
+const IndexPage = ({ allPostsData }: Posts) => {
   return (
     <Layout title="Create React App" home>
       <h1 className="text-4xl">
         Read <Link href="/posts/first-post"><a>this page</a></Link> 
       </h1>
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br/>
+            {id}
+            <br/>
+            {date}
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
---
+++ next-ts/src/types/types.ts
@@ -0,0 +1,14 @@
+export type MatterData = {
+  title: string
+  date: string
+}
+
+export type Post = {
+  id: string
+  title: string
+  date: string
+}
+
+export type Posts = {
+  allPostsData: Post[]
+}
